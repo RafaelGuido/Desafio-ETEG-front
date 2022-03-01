1ed9304ec5780cbd45e7663848d6e211d8e37428
RafaelGuido <rafatocco7@gmail.com>
2022-03-01 19:46:06 -0300

    :monocle_face: Testando url

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL = process.env.REACT_APP_API_URL
 
 export const httpClient = axios.create({
-    baseURL: baseURL,
+    baseURL: 'https://desafioeteg-api.herokuapp.com/',
     withCredentials: true
 })
 
